e9174b7a1c0e1399e0e52193c7db17ea79060ed0
David Rowe <david.t.rowe@slalom.com>
2017-11-16 15:50:48 -0600

    Clean up styles for entry page

--- src/components/pages/HomeScreen/HomeScreen.tsx
+++ src/components/pages/HomeScreen/HomeScreen.tsx
@@ -1,4 +1,10 @@
 import * as React from 'react';
+import Grid from 'material-ui/Grid';
+import Paper from 'material-ui/Paper';
+import Typography from 'material-ui/Typography';
+import Input, { InputLabel, InputAdornment } from 'material-ui/Input';
+import Button from 'material-ui/Button';
+import { FormControl } from 'material-ui/Form';
 
 interface Props {
   createPartyValue: string;
@@ -11,26 +17,68 @@ interface Props {
 }
 
 const HomeScreen: React.SFC = (props: Props) => (
-  <div>
-    <div>
-      <input
-        value={props.createPartyValue}
-        onChange={(event) => props.updateCreatePartyValue(event.target.value)}
-        type={'text'}
-        placeholder={'Party Name'}
-      />
-      <button onClick={props.submitCreateParty}>Create Party</button>
-    </div>
-    <div>
-      <input
-        value={props.joinPartyValue}
-        onChange={(event) => props.updateJoinPartyValue(event.target.value)}
-        type={'text'}
-        placeholder={'Party Name'}
-      />
-      <button onClick={props.submitJoinParty}>Join Party</button>
-    </div>
-  </div>
+  <Grid
+    container={true}
+    direction={'row'}
+    justify={'center'}
+    alignItems={'center'}
+    style={{ height: '100vh', backgroundColor: '#f5f5f5', overflowY: 'hidden' }}
+  >
+    <Grid item={true}>
+    <Paper
+      style={{ padding: 16 }}
+    >
+      <div style={{ textAlign: 'center' }}>
+        <Typography type="title"gutterBottom={true}>
+          Codenames Online
+        </Typography>
+      </div>
+      <div style={{ textAlign: 'center', paddingTop: 16 }}>
+        <FormControl>
+          <InputLabel htmlFor="join">Join Party</InputLabel>
+          <Input
+            id="join"
+            type={'text'}
+            placeholder={'Party Name'}
+            onChange={(event) => props.updateJoinPartyValue(event.target.value)}
+            endAdornment={(
+              <InputAdornment position="end">
+                <Button
+                  onClick={props.submitJoinParty}
+                  disabled={props.joinPartyValue === ''}
+                >
+                  Join
+                </Button>
+              </InputAdornment>
+            )}
+          />
+        </FormControl>
+      </div>
+      <div style={{ textAlign: 'center', paddingTop: 32 }}>
+        <FormControl>
+          <InputLabel htmlFor="start">Start Party</InputLabel>
+          <Input
+            id="start"
+            type={'text'}
+            placeholder={'Party Name'}
+            value={props.createPartyValue}
+            onChange={(event) => props.updateCreatePartyValue(event.target.value)}
+            endAdornment={(
+              <InputAdornment position="end">
+                <Button
+                  onClick={props.submitCreateParty}
+                  disabled={props.createPartyValue === ''}
+                >
+                  Create
+                </Button>
+              </InputAdornment>
+            )}
+          />
+        </FormControl>
+      </div>
+    </Paper>
+    </Grid>
+  </Grid>
 );
 
 export default HomeScreen;
